db9c6ee297ebf7f97b8d4bd8f736380d6fe86419
Coner Murphy <coner@conermurphy.com>
2022-07-06 09:13:03 +0100

    chore: updated eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,121 +1,99 @@
 module.exports = {
-    globals: {
-        React: true,
-        JSX: true,
-    },
-    env: {
-        browser: true,
-        node: true,
-        jquery: true,
-        jest: true,
-    },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    browser: true,
+    node: true,
+    jquery: true,
+    jest: true,
+  },
   extends: [
-      "airbnb-typescript",
-      "prettier",
-      "next/core-web-vitals",
-      "plugin:jest/recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    'airbnb',
+    'prettier',
+    'next/core-web-vitals',
+    'plugin:jest/recommended',
   ],
-  plugins: [
-      "jest",
-      "prettier",
-      "html",
-      "react-hooks"
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      extends: [
+        'airbnb-typescript',
+        'prettier',
+        'next/core-web-vitals',
+        'plugin:jest/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      rules: {
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          {
+            checksVoidReturn: false,
+          },
+        ],
+        '@typescript-eslint/no-unused-vars': [1, { ignoreRestSiblings: true }],
+        '@typescript-eslint/no-redeclare': [
+          'warn',
+          {
+            ignoreDeclarationMerge: true,
+          },
+        ],
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/comma-dangle': ['off'],
+      },
+    },
   ],
-  parser: '@typescript-eslint/parser',
+  plugins: ['jest', 'prettier', 'html', 'react-hooks'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
-      "project": [
-          "./tsconfig.json"
-      ]
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
   },
   rules: {
-    '@typescript-eslint/no-misused-promises': [
-        'error',
-        {
-          checksVoidReturn: false,
+    'no-redeclare': 'off',
+    'no-unused-vars': 0,
+    'no-unused-expressions': [
+      1,
+      {
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      },
+    ],
+    'react/prop-types': 0,
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
         },
-      ],
-      '@typescript-eslint/no-unused-vars': [1, { ignoreRestSiblings: true }],
-      'no-redeclare': 'off',
-      '@typescript-eslint/no-redeclare': [
-        'warn',
-        {
-          ignoreDeclarationMerge: true,
-        },
-      ],
-      '@typescript-eslint/no-floating-promises': 'off',
-      'no-unused-vars': 0,
-      "quotes": [
-          2,
-          "single",
-          {
-              "avoidEscape": true
-          }
-      ],
-      "no-unused-expressions": [
-          1,
-          {
-              "allowTernary": true
-          }
-      ],
-      "arrow-body-style": [
-          "error",
-          "always"
-      ],
-      "prettier/prettier": [
-          "error",
-          {
-              "trailingComma": "es5",
-              "singleQuote": true,
-              "printWidth": 80,
-              "endOfLine": "auto"
-          }
-      ],
-      "react/prop-types": 0,
-      "jsx-a11y/label-has-associated-control": [
-          "error",
-          {
-              "required": {
-                  "some": [
-                      "nesting",
-                      "id"
-                  ]
-              }
-          }
-      ],
-      "jsx-a11y/label-has-for": [
-          "error",
-          {
-              "required": {
-                  "some": [
-                      "nesting",
-                      "id"
-                  ]
-              }
-          }
-      ],
-      "template-curly-spacing": "off",
-      "indent": "off",
-      "no-console": "error",
-      "import/no-extraneous-dependencies": [
-          "error",
-          {
-              "devDependencies": [
-                  "**/*.test.ts",
-                  "**/*.spec.ts",
-                  "**/*.test.tsx",
-                  "**/*.spec.tsx",
-                  "jest.setup.ts"
-              ]
-          }
-      ],
-      "no-shadow": "off",
-      "@typescript-eslint/no-shadow": [
-          "error"
-      ],
-      'no-debugger': 0,
-    'no-use-before-define': "off",
+      },
+    ],
+    'template-curly-spacing': 'off',
+    indent: 'off',
+    'no-console': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.test.ts',
+          '**/*.spec.ts',
+          '**/*.test.tsx',
+          '**/*.spec.tsx',
+          'jest.setup.ts',
+        ],
+      },
+    ],
+    'no-debugger': 0,
+    'no-use-before-define': 'off',
     'no-alert': 0,
     'no-await-in-loop': 0,
     'no-return-assign': ['error', 'except-parens'],
@@ -125,13 +103,6 @@ module.exports = {
       'LabeledStatement',
       'WithStatement',
     ],
-    'no-unused-vars': [
-      1,
-      {
-        ignoreRestSiblings: true,
-        argsIgnorePattern: 'res|next|^err',
-      },
-    ],
     'prefer-const': [
       'error',
       {
@@ -139,21 +110,14 @@ module.exports = {
       },
     ],
     'arrow-body-style': [2, 'as-needed'],
-    'no-unused-expressions': [
-      2,
-      {
-        allowTaggedTemplates: true,
-      },
-    ],
     'no-param-reassign': [
       2,
       {
         props: false,
       },
     ],
-    'no-console': 0,
     'import/prefer-default-export': 0,
-    'import': 0,
+    import: 0,
     'func-names': 0,
     'space-before-function-paren': 0,
     'comma-dangle': 0,
@@ -218,7 +182,6 @@ module.exports = {
     ],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/comma-dangle': ['off'],
     'react/jsx-props-no-spreading': 'off',
-  }
-}
+  },
+};

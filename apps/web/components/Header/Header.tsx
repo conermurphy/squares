b65bd3da84629ac62a779781ba2016673171298e
Coner Murphy <coner@conermurphy.com>
2022-07-18 22:25:52 +0100

    chore(#16): marketing website - removed fixed styling

--- apps/web/components/Header/Header.tsx
+++ apps/web/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import CustomLink from '../CustomLink/CustomLink';
 
 export default function Header(): JSX.Element {
   return (
-    <header className="fixed w-full flex flex-row items-center justify-between py-8 md:px-20 max-w-screen-xl">
+    <header className="w-full flex flex-row items-center justify-between py-8 md:px-20 max-w-screen-xl">
       <Link href="/" passHref>
         <a className="font-heading text-text text-2xl md:text-3xl">Squares</a>
       </Link>
